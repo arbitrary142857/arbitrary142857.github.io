1a4230dfd7cccef615388ecad60e7eda0e289d71
Jason Mao <jasonmao@mit.edu>
2026-05-25 22:06:52 -0400

    18.701: minor fixes

--- courses/18.701/toc-sections.ts
+++ courses/18.701/toc-sections.ts
@@ -11,8 +11,8 @@ export interface TocPart {
 export const TOC_PARTS: TocPart[] = [
   { title: "Introductory Group Theory", from: 1, to: 4 },
   { title: "Fields and Vector Spaces", from: 5, to: 8 },
-  { title: "Geometric Group Theory", from: 9, to: 12 },
-  { title: "Group Actions", from: 13, to: 15 },
+  { title: "Geometric Group Theory", from: 9, to: 11 },
+  { title: "Group Actions", from: 12, to: 15 },
   { title: "Bilinear Forms", from: 16, to: 19 },
   { title: "Matrix Groups", from: 20, to: 22 },
 ];
