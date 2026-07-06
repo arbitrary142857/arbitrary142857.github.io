d128c50c98f3310953dd0bbed9bce001b7af2b4d
Jason Mao <jmao142857@gmail.com>
2026-07-06 02:29:06 -0400

    6.300: Write lecture 17 notes

--- courses/6.300/toc-sections.ts
+++ courses/6.300/toc-sections.ts
@@ -11,6 +11,7 @@ export interface TocPart {
   export const TOC_PARTS: TocPart[] = [
     { title: "Fourier Analysis", from: 1, to: 7 },
     { title: "Systems and Convolution", from: 8, to: 11 },
-    { title: "The Discrete Fourier Transform", from: 12, to: 16},
+    { title: "The Discrete Fourier Transform", from: 12, to: 16 },
+    { title: "2D Fourier Transforms", from: 17, to: 17 }
   ];
   
