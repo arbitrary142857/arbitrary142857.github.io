e25807172e501592a05f34c6fc63ef38979f3d3f
Jason Mao <jmao142857@gmail.com>
2026-08-15 23:29:00 -0400

    18.650: Write L22 notes

--- courses/18.650/toc-sections.ts
+++ courses/18.650/toc-sections.ts
@@ -12,6 +12,6 @@ export interface TocPart {
     { title: "Random Variables", from: 1, to: 6 },
     { title: "Estimators", from: 7, to: 14 },
     { title: "Hypothesis Testing", from: 15, to: 20 },
-    { title: "Bayesian Inference", from: 21, to: 21 }
+    { title: "Bayesian Inference", from: 21, to: 22 }
   ];
   
