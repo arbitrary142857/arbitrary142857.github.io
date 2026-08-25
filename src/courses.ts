ab810e6bfdef37999ad5ac876ee098c67dd37077
Jason Mao <jmao142857@gmail.com>
2026-08-25 02:44:09 -0400

    6.790: Write L9 notes

--- src/courses.ts
+++ src/courses.ts
@@ -73,7 +73,7 @@ export const COURSES: Course[] = [
     subtitle: "Machine Learning",
     semester: "☀️ Summer 2026",
     summary: `
-      Notes taken through Lecture 7 of 24.
+      Notes taken through Lecture 9 of 24.
     `,
   },
 ];
